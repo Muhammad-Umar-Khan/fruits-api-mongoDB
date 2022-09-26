addcfff8168875c1f0898cc9b041ac33309f77aa
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2022-09-26 12:09:29 +0500

    final touches

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const router = express.Router();
 const fruitController = require("./controllers/fruitController");
-const Fruit = require("./models/fruits-model");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
